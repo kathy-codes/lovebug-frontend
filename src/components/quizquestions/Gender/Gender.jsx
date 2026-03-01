b4d239038eb020af051809588ea43e06ed69659e
Kathy Yeung <kathyyeung7@gmail.com>
2026-03-01 10:09:57 -0500

    added a logic in the handleSubmit to reset the responses after submitting

--- src/components/quizquestions/Gender/Gender.jsx
+++ src/components/quizquestions/Gender/Gender.jsx
@@ -1,14 +1,14 @@
 import "./Gender.scss";
 
 const Gender = ({ gender, setGender }) => {
-    const genders = ['Nonbinary', 'Male', 'Female', 'Other'];
-    
+    const genders = ['Non-binary', 'Man', 'Woman', 'Other'];
+
     return (
         <div className="gender-container">
             <h2>What is your gender?</h2>
             <div className="gender-options-list">
                 {genders.map((g) => (
-                    <button 
+                    <button
                         key={g}
                         type="button"
                         className={`gender-button ${gender === g ? 'selected' : ''}`}
