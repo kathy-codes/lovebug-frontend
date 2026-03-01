a0bd54b1efb248e212fce300652bf769419969be
Kathy Yeung <kathyyeung7@gmail.com>
2026-02-28 21:21:42 -0500

    updated footer links on the results page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import xIcon from '../../assets/icons/x.svg';
 import instagramIcon from '../../assets/icons/instagram.svg';
 import facebookIcon from '../../assets/icons/facebook.svg';
@@ -8,6 +9,21 @@ import Typography from '../Typography/Typography';
 import './Footer.scss';
 
 const Footer = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleLinkClick = (e, hash) => {
+        e.preventDefault();
+        if (location.pathname !== '/') {
+            navigate(`/${hash}`);
+        } else {
+            const element = document.getElementById(hash.replace('#', ''));
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    };
+
     return (
         <footer className="footer" id="about">
             <div className="footer__container">
@@ -24,30 +40,30 @@ const Footer = () => {
                     <div className="footer__column">
                         <Typography variant="h4" className="footer__column-title">Product</Typography>
                         <ul className="footer__link-list">
-                            <li><a href="#how-it-works">How it Works</a></li>
-                            <li><a href="#compatibility">Compatibility</a></li>
-                            <li><a href="#pricing">Pricing</a></li>
-                            <li><a href="#success">Success Stories</a></li>
+                            <li><a href="#how-it-works" onClick={(e) => handleLinkClick(e, '#how-it-works')}>How it Works</a></li>
+                            <li><a href="#compatibility" onClick={(e) => handleLinkClick(e, '#compatibility')}>Compatibility</a></li>
+                            <li><a href="#pricing" onClick={(e) => handleLinkClick(e, '#pricing')}>Pricing</a></li>
+                            <li><a href="#success" onClick={(e) => handleLinkClick(e, '#success')}>Success Stories</a></li>
                         </ul>
                     </div>
 
                     <div className="footer__column">
                         <Typography variant="h4" className="footer__column-title">Company</Typography>
                         <ul className="footer__link-list">
-                            <li><a href="#about">About Us</a></li>
-                            <li><a href="#careers">Careers</a></li>
-                            <li><a href="#blog">Blog</a></li>
-                            <li><a href="#press">Press</a></li>
+                            <li><a href="#about" onClick={(e) => handleLinkClick(e, '#about')}>About Us</a></li>
+                            <li><a href="#careers" onClick={(e) => handleLinkClick(e, '#careers')}>Careers</a></li>
+                            <li><a href="#blog" onClick={(e) => handleLinkClick(e, '#blog')}>Blog</a></li>
+                            <li><a href="#press" onClick={(e) => handleLinkClick(e, '#press')}>Press</a></li>
                         </ul>
                     </div>
 
                     <div className="footer__column">
                         <Typography variant="h4" className="footer__column-title">Support</Typography>
                         <ul className="footer__link-list">
-                            <li><a href="#help">Help Center</a></li>
-                            <li><a href="#safety">Safety</a></li>
-                            <li><a href="#privacy">Privacy</a></li>
-                            <li><a href="#terms">Terms</a></li>
+                            <li><a href="#help" onClick={(e) => handleLinkClick(e, '#help')}>Help Center</a></li>
+                            <li><a href="#safety" onClick={(e) => handleLinkClick(e, '#safety')}>Safety</a></li>
+                            <li><a href="#privacy" onClick={(e) => handleLinkClick(e, '#privacy')}>Privacy</a></li>
+                            <li><a href="#terms" onClick={(e) => handleLinkClick(e, '#terms')}>Terms</a></li>
                         </ul>
                     </div>
                 </div>
